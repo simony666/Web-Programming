0a56bfb20efbcaa73a77c91a9db7cff6e1a4eb66
Simon Yong <51854826+simony666@users.noreply.github.com>
2023-12-29 20:05:09 +0800

    Fix redirect location

--- _/customerLayout/_head.php
+++ _/customerLayout/_head.php
@@ -22,7 +22,7 @@
         <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="../../_/customerLayout/index.php">Home</a>
+              <a class="nav-link" href="../../cart/index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="../../cart/shop.php">Shop</a>
