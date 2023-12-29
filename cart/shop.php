835b6cdaac6080be6a68cded12eb09be30edbfc5
Zzz-anonymous <firsttry2468@gmail.com>
2023-12-29 10:38:16 +0800

    modify shop.php ver2.0

--- cart/shop.php
+++ cart/shop.php
@@ -1,157 +1,140 @@
 <?php 
     include('../_base.php'); 
 
-    // use the search section
-  if(req('search')){
-    $category = req('category');
-    $price = req('price');
-    
-    $stm = $db->prepare(
-        "SELECT p.*, c.category_type
-        FROM products  AS p,
-        categories AS c
-        WHERE p.category_id = c.category_id
-        AND c.category_type = ?
-        AND product_price <= ?
-      ");
+    // Paging (class)
+    $page = req('page',1);
+    $page = max($page,1);
 
-    $stm->execute([$category,$price]);
-    $products = $stm->fetchAll();
+    require_once '../lib/Pager.php';
+    $page = new Pager('SELECT * FROM products', [], 15, $page);
+    $arr = $page->result;
 
-    // return all products
-  }else{
-    $stm = $db->prepare(
-      " SELECT *
-        FROM products  
-      ");
+    $category_id = 'CAF01';
+    // if user uses search features
+    // if (post('search')) {
+    if (is_post()) {
+      $category_id = req('category_id');
+      $price = req('price');
 
-      $stm->execute();
-      $products = $stm->fetchAll();
-  }
+      // Check if search criteria are provided
+      if (!$category_id && $price) {
+          // Search criteria are empty, return all products
+          $products = $db->query("SELECT * FROM products")->fetchAll();
+      } else {
+          $stm = $db->prepare(
+            "SELECT p.*, c.category_id
+            FROM products AS p
+            JOIN categories AS c ON p.category_id = c.category_id
+            WHERE c.category_id = ?
+            AND product_price <= ?
+        ");
+
+        $stm->execute([$category_id, $price]);
+        $products = $stm->fetchAll();
+      }
+    } else {
+      // User didn't use search features, return all products
+      $products = $db->query("SELECT * FROM products")->fetchAll();
+    }
 
       include('../_/customerLayout/_head.php');
 ?>
 
-<!-- Search -->
-<section id="search" class="my-5 py-5 ms-2">
-  <div class="container mt-5 py-5">
-    <p>Search Products</p>
-    <hr>
-  </div>
-<!-- get -->
-    <form action="shop.php">
-        <div class="mx-auto container row">
-          <div class="col-lg-12 col-md-12 col-sm-12">
-            <p>Category</p>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="category" id="category_one" value="Shirts">
-                <label for="flexRadioDefault2" class="form-check-label">
-                  Shirts
-                </label>
+
+
+  <!-- Search -->
+<div class="shop">
+    <section class="my-5 py-5 ms-2">
+    <div class="container mt-5 py-5 search-product">
+      <h4>Search Products</h4>
+      <hr>
+      <form method="post" >
+          <div class="mx-auto container row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <label class="fw-bold">Category</label>
+                <div class="form-check search">
+                  <?php
+                    $items = $db->query(
+                      "SELECT category_id, category_name
+                      FROM categories 
+                    ")->fetchAll(PDO::FETCH_KEY_PAIR);
+                    ?>
+                  <div>
+                    <?= radios('category_id', $items,true);?>
+                  </div>
               </div>
-
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="category" id="category_two" value="Pants" checked>
-                <label for="flexRadioDefault2" class="form-check-label">
-                  Pants
-                </label>
-              </div>
-
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="category" id="category_two" value="Shoes" checked>
-                <label for="flexRadioDefault2" class="form-check-label">
-                  Shoes
-                </label>
-              </div>
-
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="category" id="category_two" value="Watches" checked>
-                <label for="flexRadioDefault2" class="form-check-label">
-                  Watches
-                </label>
-              </div>
-
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="category" id="category_two" value="Skirts" checked>
-                <label for="flexRadioDefault2" class="form-check-label">
-                  Skirts
-                </label>
-              </div>
-
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="category" id="category_two" value="Jackets" checked>
-                <label for="flexRadioDefault2" class="form-check-label">
-                  Jackets
-                </label>
-              </div>
-
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="category" id="category_two" value="Bags" checked>
-                <label for="flexRadioDefault2" class="form-check-label">
-                  Bags
-                </label>
-              </div>
-          </div>
-        </div>
-
-        <div class="row mx-auto container mt-5">
-          <div class="col-lg-12 col-md-12 col-sm-12">
-
-            <p>Price</p>
-            <input type="range" class="form-range w-50" min="1" max="1000" id="customerRange2" name="price" value="100">
-            <div class="w-50">
-              <span style="float:left;">1</span>
-              <span style="float:right;">2000</span>
             </div>
           </div>
+
+          <div class="row mx-auto container mt-5">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <p class="fw-bold">Price</p>
+              <div class="slider">
+                <input type="range" class="form-range w-50 search" min="50" max="2500" id="customerRange2" name="price" value="<?= $price ?? 50 ?>" oninput="rangeValue.innerText = this.value">
+              <!-- <div class="w-50">
+                <span style="float:left;">50</span>
+                <span style="float:right;">2000</span>
+              </div> -->
+                <p id="rangeValue">100</p>
+              </div>
+              
+            </div>
+          </div>
+
+          <div class="form_group my-3 mx-3">
+            <input type="submit" name="search" value="Search" class="search-btn btn-primary">
+            <!-- <button>Submit</button> -->
+          </div>
         </div>
+      </form>
+  </section>
 
-        <div class="form_grop my-3 mx-3">
-          <input type="submit" name="search" value = Search class="btn btn-primary">
-        </div>
-    </form>
-</section>
-
-
-<!-- Shops -->
-<section id="shop" class="my-5 pb-5" >
-    <div class="container text-center py-5" style="margin-top: 4.5rem;">
-      <h3>Our Products</h3>
-      <hr class="mx-auto">
-      <p>Here you can check out our featured products</p>
-    </div>
-
-    <?php foreach($products as $p ):?>
-    <div class="row mx-auto container">
-      <!-- onclick="window.location.href='single_product.php'" -->
-      <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-        <img src="../_/photos/products/<?=  $p->product_image ?>" alt="" class="img-fluid mb-3">
-        <div class="star">
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-          <i class="fas fa-star"></i>
-        </div>
-        <h5 class="p-name"><?= $p->product_name?></h5>
-        <h4 class="p-price"><?= $p->product_price?></h4>
-        <a class="btn shop-buy-btn" href="<?= "single_product.php?product_id=".$p->product_id ?>">Buy Now</a>
+  <!-- Shops -->
+  <section id="shop" class="my-5 pb-5" >
+      <div class="container text-center py-5 mt-5 " >
+        <h3>Our Products</h3>
+        <hr class="mx-auto">
+        <p>Here you can check out our featured products</p>
       </div>
-  
-      <?php endforeach?>
+
+      <div class="row mx-auto container-fluid" id="target">
+      <?php foreach ($products as $p):?>
+        <div class="product text-center col-lg-3 col-md-4 col-sm-12" >
+          <a href="single_product.php?product_id=<?=  $p->product_id ?>">
+            <img src="../_/photos/products/<?=  $p->product_image ?>" alt="" class="img-fluid mb-3">
+            <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <h5 class="p-name"><?=  $p->product_name ?></h5>
+            <h4 class="p-price">RM<?=  $p->product_price ?></h4>
+            <button class="buy-btn">Buy Now</button>
+          </a>
+        </div>
+      <?php endforeach; ?>
+      </div>
 
       <nav aria-label="Page navigation example">
-        <ul class="pagination mt-5">
-            <li class="page-item"><a href="#" class="page-link">Previous</a></li>
-            <li class="page-item"><a href="#" class="page-link">1</a></li> 
-            <li class="page-item"><a href="#" class="page-link">2</a></li>
-            <li class="page-item"><a href="#" class="page-link">3</a></li>
-            <li class="page-item"><a href="#" class="page-link">Next</a></li>
-            
-        </ul>
+        <?= $page->html() ?>
       </nav>
-
-    </div>
   </section>
+</div>
+  
+
+
+  
+
+<script>
+    // TODO: AJAX
+    $(document).on('submit','form', e=>{
+        e.preventDefault();
+        const param = $(e.target).serializeArray();
+        console.log(param);
+        $('#target').load(' #target >', param);
+    });
+</script>
 
 <?php include('../_/customerLayout/_foot.php');?>
