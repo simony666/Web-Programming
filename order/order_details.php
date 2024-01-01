db66ef80b1955f1e08d14a1378f176896432da62
Zzz-anonymous <firsttry2468@gmail.com>
2024-01-01 17:13:42 +0800

    modify $product_details to $order_details

--- order/order_details.php
+++ order/order_details.php
@@ -4,28 +4,30 @@
 
     if(req('order_details_btn') && req('order_id')){
         $order_id = post("order_id");
-
+        
 
         // order details products part
         $stm = $db->prepare(
-            "SELECT o.*, p.*
-            FROM order_items AS o
-            JOIN products AS p 
-            ON o.product_id = p.product_id
-            WHERE o.order_id = ?
+            "SELECT oi.*,o.* 
+            FROM order_items oi, orders o 
+            WHERE o.order_id = oi.order_id 
+            AND o.order_id = ?;
             ");
 
         $stm->execute([$order_id]);
-        $product_details = $stm->fetchAll();
-        
-        // order details total_cost + count part
-        $stm = $db->prepare(
-            "SELECT total_cost, count
-            FROM orders
-            WHERE order_id = ?
-        ");
-        $stm->execute([$order_id]);
         $order_details = $stm->fetchAll();
+        
+        // get shipping address
+        $stm = $db->prepare(
+            "SELECT address, postal, state
+            FROM shipping_address
+            WHERE order_id = ?
+            ");
+
+        $stm->execute([$order_id]);
+        $addr = $stm->fetch();
+        $address = $addr->address.", ".$addr->postal." ".$_states[$addr->state];
+
     }else{
          redirect("order_history.php");
     }
@@ -39,18 +41,31 @@
         <h2 class="font-weight-bold text-center">Order Details</h2>
         <hr class="mx-auto">
     </div>
+    <div class="row ">
+        <table class="mt-5 pt-5  col">
+            <tr>
+                <th class="text-center">Shipping address</th>
+                <td class="text-center"><?= $address?></td>
+            </tr>
+        </table>
+        <div class="col"></div>
+    </div>
     <table class="mt-5 pt-5 mx-auto">
         <thead>
-            <th>Product</th>
-            <th>Price (RM)</th>
-            <th>Quantity</th>
+            <tr>
+                <th>Product</th>
+                <th>Price (RM)</th>
+                <th>Quantity</th>
+            </tr>
         </thead>
         <tbody>
-            <?php  foreach ($product_details as $p): ?>
+            <?php  foreach ($order_details as $o){ 
+                    $p = get_product($o->product_id);
+                ?>
                 <tr>
                     <td>
                         <div class="product-info">
-                            <img src="../_/photos/products/<?= $p->product_image ?>" alt="">
+                            <img src="../_/photos/products/<?= $p->photos[0] ?>" alt="">
                             <div>
                                 <p class="mt-3"><?= $p->product_name ?></p>
                             </div>
@@ -60,19 +75,18 @@
                         <span><?= $p->product_price ?></span>
                     </td>
                     <td>
-                        <span><?= $p->unit ?></span>
+                        <span><?= $o->unit ?></span>
                     </td>
                 </tr>
-            <?php endforeach; ?>
+            <?php } ?>
         </tbody>     
         <tfoot>
-            <?php  foreach ($order_details as $o): ?>
-                <tr style="border-top:3px solid #fb774b;">
-                    <td class="text-end fw-bold">Total: </td>
-                    <td>RM <?= sprintf('%.2f',$o->total_cost) ?></td>
-                    <td><?= $o->count ?></td>
-                </tr>
-            <?php endforeach; ?>
+            <tr style="border-top:3px solid #fb774b;">
+                <td class="text-end fw-bold">Total: </td>
+                <td>RM <?= sprintf('%.2f',$o->total_cost) ?>
+            </td>
+                <td><?= $o->count ?></td>
+            </tr>
         </tfoot>
     </table>
 </section>
